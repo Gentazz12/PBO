e322e9f7239d20baefc265163cf5b24e40cc0e7e
Rafly Genta Pratama <115882552+Gentazz12@users.noreply.github.com>
2024-03-10 16:13:56 +0800

    Add files via upload

---
+++ posttest 1/src/autowin/Karyawan.java
@@ -0,0 +1,43 @@
+package autowin;
+
+public class Karyawan {
+    int id;
+    String nama;
+    String posisi;
+
+    public Karyawan(int id, String nama, String posisi) {
+        this.id = id;
+        this.nama = nama;
+        this.posisi = posisi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPosisi() {
+        return posisi;
+    }
+
+    public void setPosisi(String posisi) {
+        this.posisi = posisi;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nama: " + nama + ", Posisi: " + posisi;
+    }
+
+}
